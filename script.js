23ca655e62baf63c071e564d10cfd9e32ef6d9a0
svluca <subs.vluca@gmail.com>
2026-01-22 00:22:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const question = document.getElementById("question");
 const raspunsCorect = question.dataset.answer;
+const mesajEroare = question.dataset.error;
 
 const btn = document.getElementById("btn");
 const answer = document.getElementById("answer");
-const secret = document.getElementById("secret");
+const msg = document.getElementById("msg");
 
 btn.addEventListener("click", () => {
-  if (answer.value.trim().toLowerCase() === raspunsCorect) {
+  if (
+    answer.value.trim().toLowerCase() ===
+    raspunsCorect.trim().toLowerCase()
+  ) {
     question.style.display = "none";
     secret.style.display = "block";
   } else {
-    alert("Greșit, mai încearcă ❤️");
+    msg.textContent = mesajEroare;
   }
 });
